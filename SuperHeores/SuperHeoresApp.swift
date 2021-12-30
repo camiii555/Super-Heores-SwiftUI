753c2d32003a2a18b72e0a13b8265f753d740404
juancamilomejia <juamej@eltiempo.com>
2021-12-30 15:12:13 -0500

    Initial Commit

---
+++ SuperHeores/SuperHeoresApp.swift
@@ -0,0 +1,17 @@
+//
+//  SuperHeoresApp.swift
+//  SuperHeores
+//
+//  Created by MacBook J&J  on 30/12/21.
+//
+
+import SwiftUI
+
+@main
+struct SuperHeoresApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
